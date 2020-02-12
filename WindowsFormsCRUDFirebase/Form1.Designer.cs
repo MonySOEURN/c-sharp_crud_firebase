693304dc3e8dd633e4394ad723434921a55f92df
MonySOEURN <mony.soeurn@gmail.com>
2020-02-12 04:45:14 +0000

    [TASK] get all data from the database

--- WindowsFormsCRUDFirebase/Form1.Designer.cs
+++ WindowsFormsCRUDFirebase/Form1.Designer.cs
@@ -39,6 +39,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -94,9 +99,9 @@
             this.Address.AutoSize = true;
             this.Address.Location = new System.Drawing.Point(173, 257);
             this.Address.Name = "Address";
-            this.Address.Size = new System.Drawing.Size(70, 25);
+            this.Address.Size = new System.Drawing.Size(91, 25);
             this.Address.TabIndex = 6;
-            this.Address.Text = "label3";
+            this.Address.Text = "Address";
             // 
             // Age
             // 
@@ -129,7 +134,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(362, 556);
+            this.button3.Location = new System.Drawing.Point(255, 550);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(113, 38);
             this.button3.TabIndex = 10;
@@ -137,11 +142,56 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(476, 550);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 38);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(255, 632);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(334, 38);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Delete All Item";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(795, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(995, 700);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(255, 719);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(334, 38);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Export Data to Grid View";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(887, 737);
+            this.ClientSize = new System.Drawing.Size(1868, 811);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -156,6 +206,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -174,6 +225,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- WindowsFormsCRUDFirebase/Form1.cs
+++ WindowsFormsCRUDFirebase/Form1.cs
@@ -16,6 +16,7 @@ namespace WindowsFormsCRUDFirebase
 {
     public partial class Form1 : Form
     {
+        DataTable dt = new DataTable();
         IFirebaseConfig config = new FirebaseConfig
         {
             AuthSecret = "wTmzsFqEsU6DINHgFDnexP78BrbLGFAa96Zuqfio",
@@ -37,6 +38,13 @@ namespace WindowsFormsCRUDFirebase
             {
                 MessageBox.Show("connection is established");
             }
+
+            dt.Columns.Add("ID");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Address");
+            dt.Columns.Add("Age");
+
+            dataGridView1.DataSource = dt;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -51,18 +59,30 @@ namespace WindowsFormsCRUDFirebase
 
         private async void button1_Click(object sender, EventArgs e)
         {
+
+            FirebaseResponse resp = await client.GetTaskAsync("Counter/node");
+            Counter_class get = resp.ResultAs<Counter_class>();
+
             var data = new Data
             {
-                ID = textBox1.Text,
+                ID = (Convert.ToInt32(get.cnt)+1).ToString(),
                 Name = textBox2.Text,
                 Address = textBox3.Text,
                 Age = textBox4.Text,
             };
 
-            SetResponse response = await client.SetTaskAsync("Information/" + textBox1.Text, data);
+            SetResponse response = await client.SetTaskAsync("Information/" + data.ID, data);
             Data result = response.ResultAs<Data>();
 
             MessageBox.Show("data inserted" + result.ID);
+
+            var obj = new Counter_class
+            {
+                cnt = data.ID
+            };
+
+            SetResponse response1 = await client.SetTaskAsync("Counter/node", obj);
+            
         }
 
         private async void button2_Click(object sender, EventArgs e)
@@ -90,6 +110,66 @@ namespace WindowsFormsCRUDFirebase
             FirebaseResponse response = await client.UpdateTaskAsync("Information/" + textBox1.Text, data);
             Data result = response.ResultAs<Data>();
             MessageBox.Show("Message update at ID:"+result.ID);
+
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            FirebaseResponse response = await client.DeleteTaskAsync("Information/" + textBox1.Text);
+            MessageBox.Show("Delete record of ID:" + textBox1.Text);
+        }
+
+        private async void button5_Click(object sender, EventArgs e)
+        {
+            FirebaseResponse response = await client.DeleteTaskAsync("Information");
+            MessageBox.Show("All Item has been deleted!!!");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            export();
+        }
+
+        private async void export()
+        {
+            int i = 0;
+            FirebaseResponse resp1 = await client.GetTaskAsync("Counter/node");
+            Counter_class obj1 = resp1.ResultAs<Counter_class>();
+            int cnt = Convert.ToInt32(obj1.cnt);
+
+            MessageBox.Show(cnt.ToString());
+
+            while(true)
+            {
+                if (i == cnt)
+                {
+                    break;
+                }
+                i++;
+                try
+                {
+                    FirebaseResponse resp2 = await client.GetTaskAsync("Information/"+i);
+                    Data obj2 = resp2.ResultAs<Data>();
+
+                    DataRow row = dt.NewRow();
+                    row["ID"] = obj2.ID;
+                    row["Name"] = obj2.Name;
+                    row["Address"] = obj2.Address;
+                    row["Age"] = obj2.Age;
+
+                    dt.Rows.Add(row);
+                }
+                catch
+                {
+
+                }
+            }
+            MessageBox.Show("The data is fetched!!!");
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
         }
     }
